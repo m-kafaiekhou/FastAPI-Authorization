c077355852a6f6c9cf2b4cf02ab1479dd2c48ae6
m-kafaiekhou <m.kafaiekhou@gmail.com>
2023-11-03 09:48:22 +0330

    chore: complete api views inits

--- api/v1/authorization.py
+++ api/v1/authorization.py
@@ -23,6 +23,38 @@ async def register(
     pass
 
 
+@router.post('/logout', response_model=Dict)
+async def logout(
+        token: bool = Depends(JWTBearer())
+    ):
+
+    pass
+
+
+@router.post('/reset-password', response_model=Dict)
+async def reset_password(
+        payload: CreateUserSchema = Body(),
+    ):
+
+    pass
+
+
+@router.post('/forgot-password', response_model=Dict)
+async def forgot_password(
+        payload: CreateUserSchema = Body(),
+    ):
+
+    pass
+
+
+@router.post('/forgot-password-reset', response_model=Dict)
+async def forgot_password(
+        payload: CreateUserSchema = Body(),
+    ):
+
+    pass
+
+
 @router.get('/testauth')
 async def testauth(token: bool = Depends(JWTBearer())):
     return {'message': 'Authenticated Ahmad Mohsen'}
