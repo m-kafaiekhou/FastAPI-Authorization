554730a8e27df82b2e2e3ef26589e3ae37de873f
m-kafaiekhou <m.kafaiekhou@gmail.com>
2023-11-04 21:26:13 +0330

    chore: login register and refresh view done

--- api/v1/authorization.py
+++ api/v1/authorization.py
@@ -3,6 +3,8 @@ from fastapi import Body, Depends, HTTPException, Header, APIRouter
 
 from schemas.authorization_schemas import *
 from services.authentications import JWTBearer
+from services.accounts import AccountsRequests
+from utils.auth import generate_token
 
 
 router = APIRouter()
@@ -11,8 +13,17 @@ router = APIRouter()
 async def login(
         payload: UserLoginSchema,
     ):
+    adapter = AccountsRequests()
 
-    pass
+    response = await adapter.login(payload.model_dump())
+
+    if response.status_code == 200:
+        token = await generate_token(response.json().get('user'))
+        return token
+    
+    else:
+        return response
+    
 
 
 @router.post('/register', response_model=Dict)
@@ -20,7 +31,11 @@ async def register(
         payload: CreateUserSchema = Body(),
     ):
 
-    pass
+    adapter = AccountsRequests()
+
+    response = await adapter.register(payload.model_dump())
+
+    return response
 
 
 @router.post('/logout', response_model=Dict)
@@ -31,30 +46,43 @@ async def logout(
     pass
 
 
-@router.post('/reset-password', response_model=Dict)
-async def reset_password(
-        payload: CreateUserSchema = Body(),
-    ):
-
-    pass
-
-
-@router.post('/forgot-password', response_model=Dict)
-async def forgot_password(
-        payload: CreateUserSchema = Body(),
-    ):
-
-    pass
-
-
-@router.post('/forgot-password-reset', response_model=Dict)
-async def forgot_password(
-        payload: CreateUserSchema = Body(),
-    ):
-
-    pass
+@router.get('/refresh')
+async def refresh(token: TokenSchema = Body()):
+    try:
+        payload = JWTBearer().validate_token(token)
+    except:
+        raise HTTPException(403, detail='Invalid or expired token')
+    else:
+        new_token = await generate_token(payload['user_identifier'])
+        return new_token
 
 
 @router.get('/testauth')
 async def testauth(token: bool = Depends(JWTBearer())):
     return {'message': 'Authenticated Ahmad Mohsen'}
+
+
+# @router.post('/reset-password', response_model=Dict)
+# async def reset_password(
+#         payload: CreateUserSchema = Body(),
+#     ):
+
+#     pass
+
+
+# @router.post('/forgot-password', response_model=Dict)
+# async def forgot_password(
+#         payload: CreateUserSchema = Body(),
+#     ):
+
+#     pass
+
+
+# @router.post('/forgot-password-reset', response_model=Dict)
+# async def forgot_password_reset(
+#         payload: CreateUserSchema = Body(),
+#     ):
+
+#     pass
+
+
