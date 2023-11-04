55fa2f9352620f3d90e76c442b4df378b9b50649
m-kafaiekhou <m.kafaiekhou@gmail.com>
2023-11-04 18:44:29 +0330

    chore: add jwt and redis conf to settings

--- config/settings.py
+++ config/settings.py
@@ -3,12 +3,20 @@ from pydantic_settings import BaseSettings
 
 
 class Settings(BaseSettings):
-    app_name: str = "Book API"
+    app_name: str = "Authorization Service"
     secret_key: str
     jwt_algorithm: str
 
+    jwt_access_lifetime_min: int = 60 * 24 * 7
+    jwt_refresh_lifetime_min: int = jwt_access_lifetime_min * 10
+    jwt_token_prefix: str
+
+    redis_host: str
+    redis_port: str
+
     class Config:
         env_file = '.env'
+
 
 settings = Settings()
 
